4eea43e1ac18e89138e8a2be5a3eb9b439266490
Aliktime <mamedovalik217@gmail.com>
2024-02-21 01:12:02 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,378 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+import "./animation.css";
+import runAnimations, { allLinks, allFunctions } from "./scripts";
+const App = () => {
+  useEffect(() => {
+    runAnimations();
+  }, []);
+  return (
+    <div className="parent-div">
+      <div className="home-1-1150" id="id-12302">
+        <div className="rectangle-1089-1-1295" id="id-12303"></div>
+        <div className="rectangle-1131-1-1232" id="id-12304"></div>
+        <div className="alexuihut-1-1653" id="id-12305">
+          <div className="ellipse-385-1-1035" id="id-12311"></div>
+          <div className="rectangle-1092-1-3290" id="id-12309">
+            <div className="nodeBg-12309" id="id-bg-12309">
+              {" "}
+            </div>
+          </div>
+          <div className="c-2-min-ago-1-2002" id="id-12310">
+            <span className="c-2-min-ago-1-2002-0">{"2 min ago"}</span>
+          </div>
+          <div className="group-334-1-1084" id="id-12306">
+            <div className="ellipse-386-1-174" id="id-12307"></div>
+            <div className="c-3-1-1120" id="id-12308">
+              <span className="c-3-1-1120-0">{"3"}</span>
+            </div>
+          </div>
+          <div className="text-1-1749" id="id-12312">
+            <div className="alex-linderson-1-3900" id="id-12313">
+              <span className="alex-linderson-1-3900-0">
+                {"Alex Linderson"}
+              </span>
+            </div>
+            <div className="how-are-you-tod-1-1650" id="id-12314">
+              <span className="how-are-you-tod-1-1650-0">
+                {"How are you today?"}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="team-alignuihut-1-2730" id="id-12315">
+          <div className="ellipse-304-1-1128" id="id-12319">
+            <div className="nodeBg-12319" id="id-bg-12319">
+              {" "}
+            </div>
+          </div>
+          <div className="ellipse-386-1-4888" id="id-12320">
+            <div className="nodeBg-12320" id="id-bg-12320">
+              {" "}
+            </div>
+          </div>
+          <div className="ellipse-387-1-2464" id="id-12321">
+            <div className="nodeBg-12321" id="id-bg-12321">
+              {" "}
+            </div>
+          </div>
+          <div className="ellipse-385-1-448" id="id-12323"></div>
+          <div className="c-2-min-ago-1-1083" id="id-12322">
+            <span className="c-2-min-ago-1-1083-0">{"2 min ago"}</span>
+          </div>
+          <div className="group-334-1-3696" id="id-12316">
+            <div className="ellipse-386-1-3100" id="id-12317"></div>
+            <div className="c-4-1-3132" id="id-12318">
+              <span className="c-4-1-3132-0">{"4"}</span>
+            </div>
+          </div>
+          <div className="text-1-586" id="id-12324">
+            <div className="team-align-1-4320" id="id-12325">
+              <span className="team-align-1-4320-0">{"Team Align"}</span>
+            </div>
+            <div className="dont-miss-to-at-1-1224" id="id-12326">
+              <span className="dont-miss-to-at-1-1224-0">
+                {"Don’t miss to attend the meeting."}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="abrahamuihut-1-3168" id="id-12327">
+          <div className="ellipse-385-1-4392" id="id-12330"></div>
+          <div className="rectangle-1092-1-2516" id="id-12328">
+            <div className="nodeBg-12328" id="id-bg-12328">
+              {" "}
+            </div>
+          </div>
+          <div className="c-2-min-ago-1-660" id="id-12329">
+            <span className="c-2-min-ago-1-660-0">{"2 min ago"}</span>
+          </div>
+          <div className="text-1-1452" id="id-12331">
+            <div className="john-ahraham-1-3370" id="id-12332">
+              <span className="john-ahraham-1-3370-0">{"John Ahraham"}</span>
+            </div>
+            <div className="hey-can-you-joi-1-290" id="id-12333">
+              <span className="hey-can-you-joi-1-290-0">
+                {"Hey! Can you join the meeting?"}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="saymauihut-1-408" id="id-12334">
+          <div className="ellipse-385-1-1219" id="id-12338"></div>
+          <div className="rectangle-1092-1-472" id="id-12335">
+            <div className="nodeBg-12335" id="id-bg-12335">
+              {" "}
+            </div>
+          </div>
+          <div className="rectangle-1093-1-884" id="id-12336">
+            <div className="nodeBg-12336" id="id-bg-12336">
+              {" "}
+            </div>
+          </div>
+          <div className="c-2-min-ago-1-2494" id="id-12337">
+            <span className="c-2-min-ago-1-2494-0">{"2 min ago"}</span>
+          </div>
+          <div className="text-1-3591" id="id-12339">
+            <div className="sabila-sayma-1-3569" id="id-12340">
+              <span className="sabila-sayma-1-3569-0">{"Sabila Sayma"}</span>
+            </div>
+            <div className="how-are-you-tod-1-2352" id="id-12341">
+              <span className="how-are-you-tod-1-2352-0">
+                {"How are you today?"}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="angel-daynauihu-1-1862" id="id-12342">
+          <div className="ellipse-385-1-1536" id="id-12345"></div>
+          <div className="rectangle-1094-1-530" id="id-12343">
+            <div className="nodeBg-12343" id="id-bg-12343">
+              {" "}
+            </div>
+          </div>
+          <div className="c-2-min-ago-1-1292" id="id-12344">
+            <span className="c-2-min-ago-1-1292-0">{"2 min ago"}</span>
+          </div>
+          <div className="text-1-54" id="id-12346">
+            <div className="angel-dayna-1-2352" id="id-12347">
+              <span className="angel-dayna-1-2352-0">{"Angel Dayna"}</span>
+            </div>
+            <div className="how-are-you-tod-1-1176" id="id-12348">
+              <span className="how-are-you-tod-1-1176-0">
+                {"How are you today?"}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="johnuihut-1-2560" id="id-12349">
+          <div className="ellipse-308-1-3840" id="id-12352">
+            <div className="nodeBg-12352" id="id-bg-12352">
+              {" "}
+            </div>
+          </div>
+          <div className="ellipse-385-1-2220" id="id-12354"></div>
+          <div className="rectangle-1092-1-32" id="id-12350">
+            <div className="nodeBg-12350" id="id-bg-12350">
+              {" "}
+            </div>
+          </div>
+          <div className="rectangle-1093-1-4628" id="id-12351">
+            <div className="nodeBg-12351" id="id-bg-12351">
+              {" "}
+            </div>
+          </div>
+          <div className="c-2-min-ago-1-704" id="id-12353">
+            <span className="c-2-min-ago-1-704-0">{"2 min ago"}</span>
+          </div>
+          <div className="text-1-460" id="id-12355">
+            <div className="john-borino-1-264" id="id-12356">
+              <span className="john-borino-1-264-0">{"John Borino"}</span>
+            </div>
+            <div className="have-a-good-day-1-1764" id="id-12357">
+              <span className="have-a-good-day-1-1764-0">
+                {"Have a good day 🌸"}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="status-bar-1-378" id="id-12428">
+          <div className="frame-1-3304" id="id-12442">
+            <div className="c-941-1-986" id="id-12443">
+              <span className="c-941-1-986-0">{"9:41"}</span>
+            </div>
+          </div>
+          <div className="exclude-1-4940" id="id-12429">
+            <div className="nodeBg-12429" id="id-bg-12429">
+              {" "}
+            </div>
+          </div>
+          <div className="exclude-2-819" id="id-12434">
+            <div className="nodeBg-12434" id="id-bg-12434">
+              {" "}
+            </div>
+          </div>
+          <div className="group-1-1622" id="id-12438">
+            <div className="rectangle-1-2784" id="id-12440"></div>
+            <div className="rectangle-2-3440" id="id-12441"></div>
+            <div className="vector-1-18" id="id-12439">
+              <div className="nodeBg-12439" id="id-bg-12439"></div>
+            </div>
+          </div>
+        </div>
+        <div className="tab-bar-uihut-1-2772" id="id-12358">
+          <div className="ellipse-307-1-1806" id="id-12359">
+            <div className="nodeBg-12359" id="id-bg-12359">
+              {" "}
+            </div>
+          </div>
+          <div className="home-1-1530" id="id-12364">
+            <span className="home-1-1530-0">{"Home"}</span>
+          </div>
+          <div className="group-370-1-1935" id="id-12360">
+            <div className="ellipse-308-1-222" id="id-12361"></div>
+            <div className="search-1-2752" id="id-12362">
+              <div className="vector-1-4307" id="id-12363">
+                <div className="nodeBg-12363" id="id-bg-12363"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="nav-baruihut-1-270" id="id-12403">
+          <div className="rectangle-1142-1-4648" id="id-12404"></div>
+          <div className="group-367-1-696" id="id-12413">
+            <div className="calls-1-680" id="id-12416">
+              <span className="calls-1-680-0">{"Calls"}</span>
+            </div>
+            <div className="call-1-236" id="id-12414">
+              <div className="vector-1-3441" id="id-12415">
+                <div className="nodeBg-12415" id="id-bg-12415"></div>
+              </div>
+            </div>
+          </div>
+          <div className="group-366-1-366" id="id-12417">
+            <div className="contacts-1-2886" id="id-12422">
+              <span className="contacts-1-2886-0">{"Contacts"}</span>
+            </div>
+            <div className="user-1-2236" id="id-12418">
+              <div className="ellipse-259-1-162" id="id-12419"></div>
+              <div className="ellipse-260-1-2784" id="id-12421"></div>
+              <div className="vector-1-4608" id="id-12420">
+                <div className="nodeBg-12420" id="id-bg-12420"></div>
+              </div>
+            </div>
+          </div>
+          <div className="group-365-1-0" id="id-12423">
+            <div className="settings-1-1150" id="id-12427">
+              <span className="settings-1-1150-0">{"Settings"}</span>
+            </div>
+            <div className="settings-1-1770" id="id-12424">
+              <div className="ellipse-91-1-828" id="id-12426"></div>
+              <div className="vector-1-1992" id="id-12425">
+                <div className="nodeBg-12425" id="id-bg-12425"></div>
+              </div>
+            </div>
+          </div>
+          <div className="group-368-1-4256" id="id-12405">
+            <div className="message-2-2856" id="id-12412">
+              <span className="message-2-2856-0">{"Message"}</span>
+            </div>
+            <div className="message-1-2460" id="id-12406">
+              <div className="group-1-744" id="id-12407">
+                <div className="path-1-3458" id="id-12408">
+                  <div className="nodeBg-12408" id="id-bg-12408"></div>
+                </div>
+                <div className="path-2-1050" id="id-12409">
+                  <div className="nodeBg-12409" id="id-bg-12409"></div>
+                </div>
+                <div className="path-3-3280" id="id-12410">
+                  <div className="nodeBg-12410" id="id-bg-12410"></div>
+                </div>
+                <div className="path-4-1265" id="id-12411">
+                  <div className="nodeBg-12411" id="id-bg-12411"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="storyuihut-1-260" id="id-12365">
+          <div className="storiesuihut-1-684" id="id-12366">
+            <div className="group-26-1-2170" id="id-12367">
+              <div className="adil-1-2058" id="id-12368">
+                <span className="adil-1-2058-0">{"Adil"}</span>
+              </div>
+              <div className="group-13-1-3069" id="id-12369">
+                <div className="ellipse-300-1-1376" id="id-12370">
+                  <div className="nodeBg-12370" id="id-bg-12370">
+                    {" "}
+                  </div>
+                </div>
+                <div className="ellipse-301-1-4094" id="id-12372"></div>
+                <div className="rectangle-1092-1-1254" id="id-12371">
+                  <div className="nodeBg-12371" id="id-bg-12371">
+                    {" "}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="group-27-1-1248" id="id-12387">
+              <div className="marina-1-2134" id="id-12388">
+                <span className="marina-1-2134-0">{"Marina"}</span>
+              </div>
+              <div className="group-14-1-832" id="id-12389">
+                <div className="ellipse-302-1-192" id="id-12390">
+                  <div className="nodeBg-12390" id="id-bg-12390">
+                    {" "}
+                  </div>
+                </div>
+                <div className="ellipse-303-1-0" id="id-12392"></div>
+                <div className="rectangle-1092-1-4048" id="id-12391">
+                  <div className="nodeBg-12391" id="id-bg-12391">
+                    {" "}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="group-28-1-1200" id="id-12393">
+              <div className="dean-1-108" id="id-12394">
+                <span className="dean-1-108-0">{"Dean"}</span>
+              </div>
+              <div className="group-15-1-900" id="id-12395">
+                <div className="ellipse-304-1-2809" id="id-12396">
+                  <div className="nodeBg-12396" id="id-bg-12396">
+                    {" "}
+                  </div>
+                </div>
+                <div className="ellipse-305-1-4608" id="id-12397"></div>
+              </div>
+            </div>
+            <div className="group-30-1-3472" id="id-12398">
+              <div className="max-1-1435" id="id-12399">
+                <span className="max-1-1435-0">{"Max"}</span>
+              </div>
+              <div className="group-17-1-1491" id="id-12400">
+                <div className="ellipse-308-1-2356" id="id-12401">
+                  <div className="nodeBg-12401" id="id-bg-12401">
+                    {" "}
+                  </div>
+                </div>
+                <div className="ellipse-309-1-279" id="id-12402"></div>
+              </div>
+            </div>
+            <div className="group-31-1-924" id="id-12373">
+              <div className="my-status-1-2448" id="id-12374">
+                <span className="my-status-1-2448-0">{"My status"}</span>
+              </div>
+              <div className="group-39-1-99" id="id-12375">
+                <div className="ellipse-308-1-2275" id="id-12376">
+                  <div className="nodeBg-12376" id="id-bg-12376">
+                    {" "}
+                  </div>
+                </div>
+                <div className="ellipse-301-1-2314" id="id-12377"></div>
+                <div className="group-38-1-320" id="id-12378">
+                  <div className="ellipse-307-1-626" id="id-12379"></div>
+                  <div className="ellipse-308-1-3870" id="id-12380"></div>
+                  <div className="ellipse-309-1-3170" id="id-12381"></div>
+                </div>
+                <div className="group-46-1-1550" id="id-12382">
+                  <div className="ellipse-320-1-4617" id="id-12383"></div>
+                  <div className="plus-1-322" id="id-12384">
+                    <div className="vector-1-2277" id="id-12385">
+                      <div className="nodeBg-12385" id="id-bg-12385"></div>
+                    </div>
+                    <div className="vector-2-252" id="id-12386">
+                      <div className="nodeBg-12386" id="id-bg-12386"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+ReactDOM.createRoot(document.getElementById("dualite-root")).render(<App />);
